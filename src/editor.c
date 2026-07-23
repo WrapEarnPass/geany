cbfa03689c0c85a70b9565ab63633848bb60e33c
WrapEarnPass <293701378+WrapEarnPass@users.noreply.github.com>
2026-07-23 14:09:16 -0400

    Enable switching sci layout threads at build

--- src/editor.c
+++ src/editor.c
@@ -4953,7 +4953,9 @@ static ScintillaObject *create_new_sci(GeanyEditor *editor)
 	/* Y policy is set in editor_apply_update_prefs() */
 	SSM(sci, SCI_AUTOCSETSEPARATOR, '\n', 0);
 	SSM(sci, SCI_SETSCROLLWIDTHTRACKING, 1, 0);
+#ifdef HAVE_SCI_THREAD
 	SSM(sci, SCI_SETLAYOUTTHREADS, g_get_num_processors(), 0);
+#endif
 	/* tag autocompletion images */
 	for (i = 0; i < TM_N_ICONS; i++)
 	{
