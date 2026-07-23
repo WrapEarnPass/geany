9304109640e683f7a05dd41bad87d361cb6a05d3
WrapEarnPass <293701378+WrapEarnPass@users.noreply.github.com>
2026-07-23 12:46:11 -0400

    revert e3b7d513fcc8b89f9d48bf99c29ce106334f84f3

--- scintilla/src/EditView.cxx
+++ scintilla/src/EditView.cxx
@@ -66,10 +66,6 @@
 #include "EditView.h"
 #include "ElapsedPeriod.h"
 
-#ifdef GTK
-#include <glib.h>
-#endif
-
 using namespace Scintilla;
 using namespace Scintilla::Internal;
 
@@ -394,29 +390,6 @@ void LayoutSegments(IPositionCache *pCache,
 
 }
 
-#ifdef GTK
-namespace {
-	typedef struct {
-		IPositionCache *pCache;
-		Surface *surface;
-		const ViewStyle *vstyle;
-		LineLayout **ll;
-		std::vector<TextSegment> *segments;
-		std::atomic<uint32_t> *nextIndex;
-		bool textUnicode;
-		bool multiThreaded;
-	} DecorationData;
-
-	void baked_cb(void *data, void *user_data)
-	{
-		DecorationData *decoration = (DecorationData*)data;
-		LayoutSegments(decoration->pCache, decoration->surface, *decoration->vstyle, *decoration->ll, *decoration->segments, *decoration->nextIndex, decoration->textUnicode, decoration->multiThreaded);
-		g_slice_free (DecorationData, decoration);
-	}
-}
-#endif
-
-
 /**
 * Fill in the LineLayout data for the given line.
 * Copy the given @a line and its styles from the document into local arrays.
@@ -532,25 +505,7 @@ void EditView::LayoutLine(const EditModel &model, Surface *surface, const ViewSt
 			if(threads<2){
 				LayoutSegments(pCache, surface, vstyle, ll, segments, nextIndex, textUnicode, multiThreadedContext);
 			} else { //begin threads
-#if GTK
-				GThreadPool* threadpool = g_thread_pool_new (   baked_cb,   NULL,  threads,   TRUE,   NULL );
-				for (size_t th = 0; th < threads; th++) {
-					DecorationData	*decoration = g_slice_new (DecorationData);
-					decoration->pCache=pCache;
-					decoration->surface=surface;
-					decoration->vstyle=&vstyle;
-					decoration->ll=&ll;
-					decoration->segments=&segments;
-					decoration->nextIndex=&nextIndex;
-					decoration->textUnicode=textUnicode;
-					decoration->multiThreaded=multiThreadedContext;
-					g_thread_pool_push(threadpool,decoration,NULL);
-				}
-				g_thread_pool_free(threadpool, FALSE, TRUE);
-#else
-				// If only 1 thread needed then use the main thread, else spin up multiple
-				const std::launch policy =std::launch::async;
-
+				const std::launch policy = std::launch::async;
 				std::vector<std::future<void>> futures;
 				for (size_t th = 0; th < threads; th++) {
 					// Find relative positions of everything except for tabs
@@ -563,7 +518,6 @@ void EditView::LayoutLine(const EditModel &model, Surface *surface, const ViewSt
 				for (const std::future<void> &f : futures) {
 					f.wait();
 				}
-#endif
 			}//end threads
 		}
 
